44a9f751b365d067e1cedfd0f6126310460afb55
cargamram <carlosgamram@gmail.com>
2017-01-13 12:08:10 +0100

    Primer Commit

---
+++ child1/src/main/java/Example/App.java
@@ -0,0 +1,13 @@
+package Example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Auto generated maven project" );
+    }
+}
